355b9a923a27173092ac00f89342c34ae3417970
jastipraneetkiran <jastipraneetkiran@gmail.com>
2021-07-08 07:35:56 +0530

    course app in React

---
+++ src/components/BaseComponents/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <nav>
+            <a href="/">
+                Home
+            </a>
+            |
+            <a href="/about">
+                About
+            </a>
+        </nav>
+    );
+}
